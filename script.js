5b0058b5a8542ca00a8d5072d253e47c4b1e0abc
SB Designs <hello@sbdesigns.is-a.dev>
2026-04-18 15:01:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const routes = [
-  { code: 'A1', name: 'East Croydon ↔ Croydon University Hospital', frequency: 'Every 10 mins' },
-  { code: 'B4', name: 'Purley ↔ South Croydon ↔ West Croydon', frequency: 'Every 12 mins' },
-  { code: 'C7', name: 'Norwood Junction ↔ Addiscombe ↔ New Addington', frequency: 'Every 15 mins' },
-  { code: 'N2', name: 'Night Service: Croydon ↔ Thornton Heath ↔ Crystal Palace', frequency: 'Every 30 mins' },
-  { code: 'D3', name: 'Sanderstead ↔ Lloyd Park ↔ East Croydon', frequency: 'Every 20 mins' },
-  { code: 'E8', name: 'Waddon ↔ Centrale ↔ Selhurst', frequency: 'Every 18 mins' }
+  { code: 'A1', name: 'Beddington ↔ Mitcham Common', type: 'antimony Exclusive' },
+  { code: 'A2', name: 'Thornton Heath High Street ↔ Thornton Heath Pond', type: 'antimony Exclusive' },
+  { code: 'A3', name: 'Norwood Junction ↔ Addiscombe', type: 'antimony Exclusive' },
+  { code: 'A4', name: 'Night Service: Croydon ↔ Thornton Heath', type: 'antimony Exclusive' },
+  { code: 'A', name: 'Waddon ↔ Centrale', type: 'School' }
 ];
 
 const routeGrid = document.getElementById('route-grid');
@@ -15,7 +14,7 @@ routes.forEach((route) => {
   card.innerHTML = `
     <strong>${route.code}</strong>
     <span>${route.name}</span><br>
-    <small>${route.frequency}</small>
+    <small>${route.type}</small>
   `;
   routeGrid.appendChild(card);
 });
